e9bcbe194e144299760ad893a7b35d44d9ea92c6
Jekins <isalov.alexey@gmail.com>
2022-11-10 23:30:29 +0100

    Add project files.

---
+++ Ambilight/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Ambilight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Connect = new System.Windows.Forms.Button();
+            this.Disconnect = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(139, 10);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 1;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Disconnect
+            // 
+            this.Disconnect.Location = new System.Drawing.Point(220, 10);
+            this.Disconnect.Name = "Disconnect";
+            this.Disconnect.Size = new System.Drawing.Size(75, 23);
+            this.Disconnect.TabIndex = 2;
+            this.Disconnect.Text = "Disconnect";
+            this.Disconnect.UseVisualStyleBackColor = true;
+            this.Disconnect.Click += new System.EventHandler(this.Disconnect_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(59, 39);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 236);
+            this.textBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 299);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Disconnect);
+            this.Controls.Add(this.Connect);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button Disconnect;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Ambilight/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Ambilight
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bitmap = new Bitmap(SystemInformation.VirtualScreen.Width, SystemInformation.VirtualScreen.Height);
+        SerialPort port = new SerialPort();
+        //int R = 255, G = 255, B = 255;
+        int NUM_LEDS = 30;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            try
+            {
+
+                port.Parity = Parity.None;
+                port.StopBits = StopBits.One;
+                port.DataBits = 8;
+                port.Handshake = Handshake.None;
+                port.RtsEnable = true;
+                
+                string[] ports = SerialPort.GetPortNames();
+                foreach (string port in ports)
+                {
+                    comboBox1.Items.Add(port);
+                }
+                port.BaudRate = 500000;
+                comboBox1.SelectedIndex = 1;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        
+
+
+
+        public int[] getFrame(int frame_num)
+        {
+
+            int[] frame = new int[3];
+
+            if (frame_num == 1)
+            {
+                Graphics graphics = Graphics.FromImage(bitmap as Image); // Create a new graphics objects that can capture the screen
+                graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size); // Screenshot moment → screen content to graphics object
+            }
+
+
+            int sumR = 0;
+            int sumG = 0;
+            int sumB = 0;
+
+            if (frame_num <= NUM_LEDS / 2)
+            {
+                for (int x = bitmap.Width / (NUM_LEDS / 2) * frame_num - bitmap.Width / (NUM_LEDS / 2); x < bitmap.Width / (NUM_LEDS / 2) * frame_num; x += 16)
+                {
+
+                    for (int y = 0; y < bitmap.Height / 6; y += 18)
+                    {
+
+                        byte rr = bitmap.GetPixel(x, y).R;
+                        byte gg = bitmap.GetPixel(x, y).G;
+                        byte bb = bitmap.GetPixel(x, y).B;
+
+                        sumR += rr;
+                        sumG += gg;
+                        sumB += bb;
+                    }
+                }
+            }
+
+            if (frame_num > NUM_LEDS / 2)
+            {
+                frame_num -= NUM_LEDS / 2;
+
+                for (int x = bitmap.Width / (NUM_LEDS / 2) * frame_num - bitmap.Width / (NUM_LEDS / 2); x < bitmap.Width / (NUM_LEDS / 2) * frame_num; x += 16)
+                {
+                    for (int y = bitmap.Height / 6 * 5; y < bitmap.Height; y += 18)
+                    {
+                        byte rr = bitmap.GetPixel(x, y).R;
+                        byte gg = bitmap.GetPixel(x, y).G;
+                        byte bb = bitmap.GetPixel(x, y).B;
+
+                        sumR += rr;
+                        sumG += gg;
+                        sumB += bb;
+                    }
+                }
+            }
+
+            frame[0] = sumR / ((bitmap.Width / (NUM_LEDS / 2)) / 16 * ((bitmap.Height / 6)) / 18);
+            frame[1] = sumG / ((bitmap.Width / (NUM_LEDS / 2)) / 16 * ((bitmap.Height / 6)) / 18);
+            frame[2] = sumB / ((bitmap.Width / (NUM_LEDS / 2)) / 16 * ((bitmap.Height / 6)) / 18);
+
+
+            return frame;
+
+        }
+
+
+
+        
+
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!port.IsOpen)
+                {
+                    port.PortName = comboBox1.Text;
+                    port.Open();       
+                    port.Write("c");
+                    timer1.Enabled = true;
+                    timer1.Interval = 50;
+                }
+            }
+
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public int[] GetScreen()
+        {
+            int[] screen = new int[3 * NUM_LEDS];
+            int[] screen_temp = new int[3];
+
+            for (int i = 1; i <= NUM_LEDS; i++)
+            {
+
+                screen_temp = getFrame(i);
+                screen[i * 3 - 3] = screen_temp[0];
+                screen[i * 3 - 2] = screen_temp[1];
+                screen[i * 3 - 1] = screen_temp[2];
+            }
+
+
+            return screen;
+        }
+
+
+        public void setAmbient()
+        {
+           
+            int[] screen = GetScreen();
+
+            try
+            {
+                textBox1.Text = string.Join(" ", screen);
+                port.Write(string.Join(" ", screen) + "A");
+            }
+
+            catch (Exception ex)
+            {
+               
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Disconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                port.Write("d");
+                port.Close();
+                timer1.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+                setAmbient();
+            
+        }
+    }
+}
+
